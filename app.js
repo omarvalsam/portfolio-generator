cc58e11d748b61e53009338a57884348b89bc49e
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-01-02 16:21:05 -0700

    added page creation template

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // This....
-  for (let i = 0; i < profileDataArr.length; i++){
-  console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('=================')
+const[name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem))
-  
-};
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Checkout index.html to see the output!');
+})
