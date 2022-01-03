0e2956a4ee89d324aac52c6dcbe47387e6dba38f
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-01-02 17:30:25 -0700

    added the first question

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const[name, github] = profileDataArgs;
+// const[name, github] = profileDataArgs;
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Checkout index.html to see the output!');
-})
+//   console.log('Portfolio complete! Checkout index.html to see the output!');
+// })
